d8882505af232b2f9a47abe234c2c7dfaaf6133a
Moshin Jaffar <moshin.jaffar@sourcegraph.com>
2026-01-06 17:48:39 +0700

    Add CORS middleware for frontend compatibility

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import organisation_router, employee_router, health_router
 from api.config import APP_VERSION, API_VERSION, API_PREFIX
 
 app = FastAPI(title="Organisation API", version=APP_VERSION)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(organisation_router, prefix=API_PREFIX)
 app.include_router(employee_router, prefix=API_PREFIX)
